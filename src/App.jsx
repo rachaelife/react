53ebd5c7818b74c9ace1b6fd383be257d5f3b065
rachaelife <devrachaelojo@gmail.com>
2025-12-08 13:37:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Bs0Circle } from "react-icons/bs"
+
+
+function App() {
+  
+
+  return (
+ <>
+ <h1 className="text-red-500 text-6xl font-bold">This is my first react project</h1>
+
+ <div className="flex justify-center items-center gap-10 m-10 p-10 bg-blue-200 container">
+
+  <div className="width-100 height-100 bg-amber-400">container one </div>
+  <div className="width-100 height-100 bg-white">container two </div>
+   <Bs0Circle/>
+ </div>
+</>
+  )
+}
+
+export default App

27457c6ab35b542d179d5d1c7f659c271c44255d
rachaelife <devrachaelojo@gmail.com>
2025-12-10 12:26:58 +0100

    i worked on narbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Bs0Circle } from "react-icons/bs"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import EcobazarNavbar from "./pages/components/nav"
+import Homepage from "./pages/homepage"
 
 
 function App() {
@@ -6,14 +9,11 @@ function App() {
 
   return (
  <>
- <h1 className="text-red-500 text-6xl font-bold">This is my first react project</h1>
-
- <div className="flex justify-center items-center gap-10 m-10 p-10 bg-blue-200 container">
-
-  <div className="width-100 height-100 bg-amber-400">container one </div>
-  <div className="width-100 height-100 bg-white">container two </div>
-   <Bs0Circle/>
- </div>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Homepage/>} />  
+  </Routes>
+  </BrowserRouter>
 </>
   )
 }
